9f1b004d4d32200e6bd0167f607acf8ac0466dca
Jahn Carlos Najera <jahnnajera@Jahns-MacBook-Pro.local>
2021-06-23 11:33:47 -0600

    Commit

---
+++ src/components/screens/Home.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from "react-native";
+
+function Home(){
+    return(
+        <View>
+            <Text>Bienvenidos</Text>
+        </View>
+    );
+};
+
+export default Home;
